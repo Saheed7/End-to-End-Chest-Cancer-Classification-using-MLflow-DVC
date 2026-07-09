8022a7d6f3a663f3e0ea8795e95382b223107ee2
Kayode <kayodesaheed@gmail.com>
2026-07-09 17:40:51 +0100

    utils, exception & logger added

--- template.py
+++ template.py
@@ -12,6 +12,8 @@ list_of_files=[
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
@@ -21,6 +23,8 @@ list_of_files=[
     "params.yaml",
     "requirements.txt",
     "setup.py",
+    "main.py",
+    "app.py", 
     "research/trials.ipynb",
     "templates/index.html"
     
